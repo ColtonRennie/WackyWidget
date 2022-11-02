f53e4b8c6276b3c3917d3fc94645909d4fe4e69c
Colton Rennie <colton.rennie@hotmail.com>
2022-11-01 22:41:43 -0300

    complete wackywidget

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Widget
+from .forms import WidgetForm
 
-# Create your views here.
+
+def index(request):
+  widget_list = Widget.objects.all()
+  widget_form = WidgetForm()
+  return render(request, 'index.html', {'widget_list': widget_list, 'widget_form': widget_form})
+
+
+def add_widget(request):
+  form = WidgetForm(request.POST)
+  if form.is_valid():
+    new_widget = form.save(commit=False)
+    new_widget.save()
+  return redirect('/')
+
+def delete_widget(request, widget_id):
+  Widget.objects.get(id=widget_id).delete()
+  return redirect('/')
